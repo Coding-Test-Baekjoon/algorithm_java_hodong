b5c8b8920c1b7a40ea693252645c52b9056d0fe4
SeokHoDong <71691451+SeokHoDong@users.noreply.github.com>
2025-01-01 21:45:04 +0900

    2720
    2720

---
+++ 단계별로 풀어보기-일반 수학 1/Question2720.java
@@ -0,0 +1,38 @@
+package y202501.d01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Question2720 {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+
+            int testCases = Integer.parseInt(reader.readLine());
+            StringBuilder builder = new StringBuilder();
+
+            int[] coins = {25, 10, 5, 1};
+
+            while (testCases-- > 0) {
+                int change = Integer.parseInt(reader.readLine());
+                int[] counts = new int[4];
+
+                for (int i = 0; i < coins.length; i++) {
+                    counts[i] = change / coins[i];
+                    change %= coins[i];
+                }
+
+                // 결과를 저장
+                builder.append(counts[0]).append(" ")
+                        .append(counts[1]).append(" ")
+                        .append(counts[2]).append(" ")
+                        .append(counts[3]).append("\n");
+            }
+
+            // 출력
+            System.out.print(String.valueOf(builder));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

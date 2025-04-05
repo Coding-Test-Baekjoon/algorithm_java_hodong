e8e8e10c3bad0fd8d9f5d1a70d229f0a7c00ed3b
SeokHoDong <71691451+SeokHoDong@users.noreply.github.com>
2025-04-05 23:07:22 +0900

    Create Question24263.java
    24263

---
+++ 단계별로 풀어보기/시간 복잡도/Question24263.java
@@ -0,0 +1,18 @@
+package y202504.d05;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Question24263 {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
+            int n = Integer.parseInt(reader.readLine());
+            System.out.println(n);
+            System.out.println(1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

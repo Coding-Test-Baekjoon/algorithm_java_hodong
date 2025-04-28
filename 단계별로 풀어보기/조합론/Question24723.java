5f82c266ba338e97b474b7968756ae08f832c80a
SeokHoDong <71691451+SeokHoDong@users.noreply.github.com>
2025-04-28 23:42:16 +0900

    Create Question24723.java
    24723

---
+++ 단계별로 풀어보기/조합론/Question24723.java
@@ -0,0 +1,18 @@
+package y202504.d28;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Question24723 {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
+            int n = Integer.parseInt(reader.readLine());
+            int result = (int) Math.pow(2, n);
+            System.out.println(result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
